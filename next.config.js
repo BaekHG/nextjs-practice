573979e52fd41104752b8575c4baf38302f2f470
hgbaek <hhyungi94@gmail.com>
2023-05-24 17:40:09 +0900

    apply rewrites (hide api key)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
     return [{ source: '/hello', destination: '/about', permanent: false }];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/popular',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
   },
 };
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,49 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
     <nav>
+      <img src="/vercel.svg" />
       <Link href="/">
-        <h1>Home</h1>
+        <h1 className={router.pathname === '/' ? 'active' : ''}>Home</h1>
       </Link>
       <Link href="/about">
-        <h1>About</h1>
+        <h1 className={router.pathname === '/about' ? 'active' : ''}>About</h1>
       </Link>
-
-      {/* style jsx */}
-      <style jsx>{`
-        nav {
-          background-color: tomato;
-        }
-      `}</style>
+      <style jsx>
+        {`
+          nav {
+            display: flex;
+            gap: 10px;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            padding-top: 20px;
+            padding-bottom: 10px;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+              rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+          }
+          img {
+            max-width: 100px;
+            margin-bottom: 5px;
+          }
+          nav a {
+            font-weight: 600;
+            font-size: 18px;
+          }
+          .active {
+            color: tomato;
+          }
+          nav div {
+            display: flex;
+            gap: 10px;
+          }
+        `}
+      </style>
     </nav>
   );
 }
